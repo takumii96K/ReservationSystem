23d01128dc5b78d2c038fad33656bdca576c86ec
takumi <154777096+takumii96K@users.noreply.github.com>
2024-03-08 14:28:30 +0900

    コミット

--- src/main/java/org/example/resevationsystem/controller/ProductController.java
+++ src/main/java/org/example/resevationsystem/controller/ProductController.java
@@ -1,27 +1,36 @@
 package org.example.resevationsystem.controller;
 
+import lombok.RequiredArgsConstructor;
+import org.example.resevationsystem.entity.Product;
 import org.example.resevationsystem.service.ProductService;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.List;
 
 /** Productコントローラ */
 @Controller
 @RequestMapping("/takeout/product")
+@RequiredArgsConstructor
 public class ProductController {
-	/* DI対象 */
-	@Autowired
-	ProductService service;
+
+	private final ProductService service;
+
+	@ModelAttribute
+	public void setupProductForm(Model model){
+		List<Product> products = service.getAllProducts();
+		model.addAttribute("products", products);
+
+	}
 
 	// 商品選択画面を表示
-	@GetMapping("/product")
+	@GetMapping
 	public String showProductSelection(Model model) {
-		// 商品一覧を取得してモデルに追加
-		model.addAttribute("product", service.getSelectAll());
-		return "product";
+		return "/product";
 	}
 
 	@PostMapping("/reservation")
