9705e210acab9627421e53ef210c0ba06ca6ad01
takumi <154777096+takumii96K@users.noreply.github.com>
2024-03-08 17:37:53 +0900

    コミット

--- src/main/java/org/example/reservationsystem/ReservationSystemApplication.java
+++ src/main/java/org/example/reservationsystem/ReservationSystemApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class ResevationSystemApplication {
+public class ReservationSystemApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(ResevationSystemApplication.class, args);
+        SpringApplication.run(ReservationSystemApplication.class, args);
     }
 
 }
---
+++ src/test/java/org/example/reservationsystem/ReservationSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.reservationsystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReservationSystemApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
